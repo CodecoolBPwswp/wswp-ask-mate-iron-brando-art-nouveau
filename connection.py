a396710ee1514fe89587d2b4c095948d554f20af
zslim <zsofia.limbek@gmail.com>
2018-08-21 14:39:34 +0200

    added backend functions for question adding

--- connection.py
+++ connection.py
@@ -8,5 +8,11 @@ def read_csv_to_list_of_dicts(file_path):
     return list_of_dicts
 
 
-def append_line_to_csv(file_path, line_to_append):
+def append_line_to_csv(file_path, data_header, line_to_append):
+    with open(file_path, "a") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data_header)
+        writer.writerow(line_to_append)
+
+
+def update_line_in_csv(file_path, updated_line):
     pass
--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,5 @@
 import connection
+import datetime
 import uuid
 
 FILE_PATH_TO_QUESTIONS = "questions.csv"
@@ -11,12 +12,23 @@ def get_all_questions():
     return list_of_questions
 
 
-def add_new_entry(entry_type, dict_of_new_entry):
-    """
-    Saves a new entry into the appropriate file
-    :param entry_type: "question" or "answer"
-    :param dict_of_new_entry: a dictionary that contains the values to save
-    :return: None
-    """
-    pass
+def add_new_question(dict_of_new_question):
+    # expected fields: view_number, title, message, image
+    # fields to add: id, submission_time
+    dict_of_new_question = add_id_to_entry(dict_of_new_question)
+    dict_of_new_question = add_submission_time(dict_of_new_question)
+    for field_name in HEADER_QUESTIONS:
+        if field_name not in dict_of_new_question:
+            raise KeyError(f"Missing field from new question: {field_name}")
+    connection.append_line_to_csv(FILE_PATH_TO_QUESTIONS, HEADER_QUESTIONS, dict_of_new_question)
 
+
+def add_id_to_entry(dict_of_new_entry):
+    dict_of_new_entry["id"] = uuid.uuid4()
+    return dict_of_new_entry
+
+
+def add_submission_time(dict_of_new_entry):
+    current_time_str = str(datetime.datetime.now())[:-7]
+    dict_of_new_entry["submission_time"] = current_time_str
+    return dict_of_new_entry
