2557a99669268434f3b5f0afb8f5745ffbe9c945
Istvan Zsolnai <Zsolnai.Istvan1995@gmail.com>
2018-09-05 13:20:35 +0200

    developing search bar in progress

--- data_manager.py
+++ data_manager.py
@@ -118,6 +118,7 @@ def get_search_results(cursor, keyword):
     cursor.execute("""
                     SELECT * FROM question
                     WHERE message LIKE '%%' || %(keyword)s || '%%'
+                    
                          """, {'keyword': keyword})
     search_result = cursor.fetchall()
     return search_result
