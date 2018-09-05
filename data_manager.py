07b12b24ae342fe5d492c3050cfe714074df61d0
Istvan Zsolnai <Zsolnai.Istvan1995@gmail.com>
2018-09-05 13:04:43 +0200

    Working search engine, only sees messeges sql yet.

--- data_manager.py
+++ data_manager.py
@@ -117,7 +117,7 @@ def get_answers_by_question_id(cursor, _id):
 def get_search_results(cursor, keyword):
     cursor.execute("""
                     SELECT * FROM question
-                    WHERE message LIKE '%%keyword%%'
+                    WHERE message LIKE '%%' || %(keyword)s || '%%'
                          """, {'keyword': keyword})
     search_result = cursor.fetchall()
     return search_result
