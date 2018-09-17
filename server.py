b61e3ba31e8b40e4e9738babb2c4f2c9dc3d352b
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-09-17 12:53:36 +0200

    refactored server
    deleted comments from server
    
    refactored server

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 import data_manager
-import utils
 
 app = Flask(__name__)
 
@@ -16,8 +15,8 @@ def route_index():
 @app.route('/list')
 def route_list():
     is_list_truncated = False
-    user_questions = data_manager.get_all_questions()
-    return render_template('lists.html', questions=user_questions, truncated=is_list_truncated)
+    list_of_questions = data_manager.get_all_questions()
+    return render_template('lists.html', questions=list_of_questions, truncated=is_list_truncated)
 
 
 @app.route('/list/order-by/<column>/<order>')
@@ -35,7 +34,7 @@ def add_new_question():
 @app.route('/new_question', methods=['POST'])
 def save_new_question():
     dict_of_question = request.form.to_dict()
-    data_manager.add_new_question(dict_of_question)  # not working
+    data_manager.add_new_question(dict_of_question)
     question_id = data_manager.get_last_question_by_title(dict_of_question["title"])
     question_page_url = url_for("get_question_details", question_id=question_id)
     return redirect(question_page_url)
@@ -48,8 +47,8 @@ def get_question_details(question_id):
     answers_for_question = data_manager.get_answers_by_question_id(question_id)
     comments_for_question = data_manager.get_comments_by_question_id(question_id)
     comments_for_answer = data_manager.get_answer_comments_to_question(question_id)
-    return render_template("question_page.html", dict_of_question = question, comments_for_question = comments_for_question,
-                           answers_to_list = answers_for_question, comments_for_answer=comments_for_answer)
+    return render_template("question_page.html", dict_of_question=question, comments_for_question=comments_for_question,
+                           answers_to_list=answers_for_question, comments_for_answer=comments_for_answer)
 
 
 @app.route('/question-upvote', methods=["POST"])
@@ -60,7 +59,7 @@ def upvote_question():
     return redirect(url_to_voted_question)
 
 
-@app.route('/question-downvote', methods = ["POST"])
+@app.route('/question-downvote', methods=["POST"])
 def downvote_question():
     question_id = request.form["question_id"]
     data_manager.add_question_downvote(question_id)
@@ -92,27 +91,26 @@ def comment_to_question(question_id):
     form_action = url_for('post_comment_to_question', postid=question_id)
     question_to_comment = data_manager.get_question_by_id(question_id)
     return render_template("comments.html", dict_of_record=question_to_comment,
-                           distinguish = instance_to_comment, form_action=form_action)
+                           distinguish=instance_to_comment, form_action=form_action)
 
 
-@app.route('/details/<postid>/new-comment', methods = ["POST"])
-def post_comment_to_question(postid):
+@app.route('/details/<question_id>/new-comment', methods=["POST"])
+def post_comment_to_question(question_id):
     dict_of_new_comment = request.form.to_dict()
-    dict_of_new_comment["question_id"] = postid
+    dict_of_new_comment["question_id"] = question_id
     data_manager.add_new_comment(dict_of_new_comment)
-    url_to_question_details = url_for("get_question_details", question_id=postid)
+    url_to_question_details = url_for("get_question_details", question_id=question_id)
     return redirect(url_to_question_details)
 
 
-##### comment to answer
+@app.route('/answer/<answer_id>/new-comment')  # to be finished
+def comment_to_answer(answer_id):
+    form_action = url_for('post_comment_to_answer', answer_id=answer_id)
+    answer_to_comment = data_manager.get_answer_by_id(answer_id)
+    return render_template("comments.html", dict_of_record=answer_to_comment, form_action=form_action)
 
-@app.route('/answer/<postid>/new-comment')  # to be finished
-def comment_to_answer(postid):
-    form_action = url_for('post_comment_to_answer', answer_id=postid)
-    comment_to_answer = data_manager.get_answer_by_id(postid)
-    return render_template("comments.html", dict_of_record=comment_to_answer, form_action=form_action)
 
-@app.route('/answer/<answer_id>/new-comment', methods = ["POST"])
+@app.route('/answer/<answer_id>/new-comment', methods=["POST"])
 def post_comment_to_answer(answer_id):
     dict_of_new_comment = request.form.to_dict()
     dict_of_new_comment["answer_id"] = answer_id
@@ -121,14 +119,10 @@ def post_comment_to_answer(answer_id):
     url_to_question_details = url_for("get_question_details", question_id=question_id)
     return redirect(url_to_question_details)
 
-#####
 
-
-
-
-@app.route('/details/<postid>/new-answer')
-def new_answer(postid):
-    question_to_answer = data_manager.get_question_by_id(postid)
+@app.route('/details/<question_id>/new-answer')
+def new_answer(question_id):
+    question_to_answer = data_manager.get_question_by_id(question_id)
     user_action = "Add new"
     form_action = url_for("post_answer", postid=question_to_answer["id"])
     return render_template("answer.html", dict_of_question=question_to_answer,
@@ -136,7 +130,7 @@ def new_answer(postid):
                            form_action=form_action)
 
 
-@app.route('/details/<postid>/new-answer', methods = ["POST"])
+@app.route('/details/<postid>/new-answer', methods=["POST"])
 def post_answer(postid):
     dict_of_new_answer = request.form.to_dict()
     dict_of_new_answer["question_id"] = postid
@@ -165,12 +159,11 @@ def save_edited_answer():
     return redirect(url_to_question)
 
 
-@app.route('/search', methods = ["POST","GET"])
+@app.route('/search', methods=["POST", "GET"])
 def search_question():
     keyword = request.form['search']
     search_result = data_manager.get_search_results(keyword)
-    return render_template('search_results.html', keyword=keyword, search_result = search_result)
-
+    return render_template('search_results.html', keyword=keyword, search_result=search_result)
 
 
 if __name__ == "__main__":
