b9d86e102d9717259b92d028c46a7e1febd149d7
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-09-18 14:35:52 +0200

    altered table name & added sample data to users

--- sample_data/adding_user_data.sql
+++ sample_data/adding_user_data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user (
+CREATE TABLE users (
   id serial NOT NULL,
   registration_time timestamp without time zone,
   email varchar(100) UNIQUE,
@@ -8,10 +8,12 @@ CREATE TABLE user (
   reputation integer
 );
 
-ALTER TABLE ONLY user
+ALTER TABLE ONLY users
     ADD CONSTRAINT pk_user_id PRIMARY KEY (id);
 
-INSERT INTO user VALUES (0, '2012-10-04 15:22:50', 'admin@askmate.com', '$2b$12$ttSPTU82AdBnteNBVnI5z.fX62DzaPmixn3XIgcXOqGIuLpXIlEDC', 'Admin', '', 0)
-INSERT INTO user VALUES (1, '2012-10-04 15:22:50', 'sybillak@citromail.hu', '$2b$12$h9OWYSVr47m66mPrb7uUH./W1NtyHwfFyxYdU1PHFMja463fa/KOO', 'Andris', '2014-09-15 23:16:00', 20)
-INSERT INTO user VALUES (2, '2013-04-15 08:23:01', 'kiss_eszter@codemail.hu', '$2b$12$HLOZ0sriSQQltByGNPKOk.h9ysFesFN/0NeM5pMXsS1VIdVNw.D2u', 'Eszti', '2018-09-18 08:58:12', -2)
+INSERT INTO users VALUES (0, '2012-10-04 15:22:50', 'admin@askmate.com', '$2b$12$ttSPTU82AdBnteNBVnI5z.fX62DzaPmixn3XIgcXOqGIuLpXIlEDC', 'Admin', NULL, 0)
+INSERT INTO users VALUES (1, '2012-10-04 15:22:50', 'sybillak@citromail.hu', '$2b$12$h9OWYSVr47m66mPrb7uUH./W1NtyHwfFyxYdU1PHFMja463fa/KOO', 'Andris', '2014-09-15 23:16:00', 20)
+INSERT INTO users VALUES (2, '2013-04-15 08:23:01', 'kiss_eszter@codemail.hu', '$2b$12$HLOZ0sriSQQltByGNPKOk.h9ysFesFN/0NeM5pMXsS1VIdVNw.D2u', 'Eszti', '2018-09-18 08:58:12', -2)
+
+SELECT pg_catalog.setval('users_id_seq', (SELECT max(id) FROM users));
 
