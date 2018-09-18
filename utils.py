b54122d68ced572a9c30598b88d96492b898fe30
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-09-18 13:18:45 +0200

    added counter initializer & current time getter to utils

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
 import datetime
 
 
-def add_submission_time(dict_of_new_entry):
+def get_current_time():
     current_time_str = str(datetime.datetime.now())[:16]
-    dict_of_new_entry["submission_time"] = current_time_str
+    return current_time_str
+
+
+def add_submission_time(dict_of_new_entry):
+    dict_of_new_entry["submission_time"] = get_current_time()
     return dict_of_new_entry
 
 
@@ -12,3 +16,9 @@ def add_missing_fields(dict_to_check, header): # vote és view should be 0
         if field_name not in dict_to_check:
             dict_to_check[field_name] = None
     return dict_to_check
+
+
+def initialize_counter_fields(dict_of_entry, field_names):
+    for field in field_names:
+        dict_of_entry[field] = 0
+    return dict_of_entry
